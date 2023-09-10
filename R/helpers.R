7c0163d6dc163262408bc0597cf998a0e9d47027
Lee Durbin <lee.durbin@aucklandcouncil.govt.nz>
2023-09-11 08:56:40 +1200

    Added first function with associated test

---
+++ R/helpers.R
@@ -0,0 +1,23 @@
+#' Bind A List Of Dataframes As A Tibble
+#'
+#' @description Column-bind a dataframe to another dataframe whose rows are
+#'   exactly aligned, and return a tibble.
+#'
+#' @param data A dataframe whose rows are in the same order as the dataframe
+#'   passed to bind_to.
+#' @param bind_to A second dataframe whose rows are in the same order as the
+#'   dataframe passed to data.
+#' @param ... Optional key-value pairs passed to select(...).
+#'
+#' @return A tibble.
+#'
+#' @noRd
+bind_and_tibble <- function(data, bind_to, ...) {
+  bind_to <- select(bind_to, ...)
+
+  bound_data <- data |>
+    cbind(bind_to) |>
+    dplyr::as_tibble()
+
+  return(bound_data)
+}
---
+++ tests/testthat/test-helpers.R
@@ -0,0 +1,8 @@
+test_that("two dataframes are column-bound and returned as a single tibble", {
+  df_1 <- data.frame(x = 1:10, y = letters[1:10])
+  df_2 <- data.frame(z = 11:20, a = letters[11:20])
+  expected_df <- tibble::tibble(x = 1:10, y = letters[1:10], z = 11:20,
+                                   a = letters[11:20])
+
+  expect_equal(bind_and_tibble(df_1, df_2, z:a), expected_df)
+})
