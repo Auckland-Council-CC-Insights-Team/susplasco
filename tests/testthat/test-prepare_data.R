5b5e2054bb76ede0d4c7657365a56d8531db164c
Lee Durbin <lee.durbin@aucklandcouncil.govt.nz>
2023-09-18 09:05:16 +1200

    Another unit test (that passes)

--- tests/testthat/test-prepare_data.R
+++ tests/testthat/test-prepare_data.R
@@ -11,3 +11,24 @@ test_that("Google Sheet data is returned with renamed columns", {
   expect_equal(returned[c(6:8, 22)], expected)
   expect_error(returned |> select(dplyr::starts_with("q")))
 })
+
+
+test_that("multi-choice options are converted to binaries", {
+  data <- clean_google_sheet_data("live")
+
+  expect_equal(get_question_answers(data) |> ncol(), 125)
+})
+
+
+test_that("a multi-choice option is converted to binary", {
+  data <- clean_google_sheet_data("live")
+
+  binary <- is_option_selected(
+    data,
+    "tt_1f",
+    "how_do_you_connect_with_the_whenua_surrounding_natural_environment",
+    "We use our local parks and areas for events"
+    )
+
+  expect_equal(binary$tt_1f, 1)
+})
