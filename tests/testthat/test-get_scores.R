852865116743b2a7a1e0141f20c76fafbcc93ef1
Lee Durbin <lee.durbin@aucklandcouncil.govt.nz>
2023-09-19 07:04:11 +1200

    Unit test for get_status_scores, which passes

--- tests/testthat/test-get_scores.R
+++ tests/testthat/test-get_scores.R
@@ -14,3 +14,15 @@ test_that("the total number of actions selected for a given question returns as 
 
   expect_equal(score$tt_1_score, 4)
 })
+
+
+test_that("the total number of status-specific actions selected across each Pou returns as expected", {
+  data <- get_data()
+  status_scores <- get_status_scores(data)
+  total_status_scores <- status_scores |>
+    janitor::adorn_totals() |>
+    filter(metric == "Total") |>
+    pull(score)
+
+  expect_equal(total_status_scores, 41)
+})
